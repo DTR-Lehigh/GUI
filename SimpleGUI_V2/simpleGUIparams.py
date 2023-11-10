53c6756fec6116ded1246f93774087d628f07475
Hanqing Qi <billy.qi03@gmail.com>
2023-11-10 17:42:10 -0500

    🎉 Start `SimpleGUI_V2`
    Start to make gui for multi robot.

---
+++ SimpleGUI_V2/simpleGUIparams.py
@@ -0,0 +1,23 @@
+"""
+Author       : Hanqing Qi
+Date         : 2023-11-10 17:40:20
+LastEditors  : Hanqing Qi
+LastEditTime : 2023-11-10 17:40:21
+FilePath     : /GUI/SimpleGUI_V2/simpleGUIparams.py
+Description  : Parameters for the simpleGUI.py
+"""
+
+# Colors
+COLORS = {
+    "red": "#FF5555",
+    "green": "#50FA7B",
+    "yellow": "#F1FA8C",
+    "purple": "#BD93F9",
+    "orange": "#FFB86C",
+    "cyan": "#8BE9FD",
+    "pink": "#FF79C6",
+    "white": "#F8F8F2",
+    "black": "#282A36",
+    "gray": "#44475A",
+    "drak_blue": "#6272A4",
+}
---
+++ SimpleGUI_V2/simpleGUIutils.py
@@ -0,0 +1,18 @@
+"""
+Author       : Hanqing Qi
+Date         : 2023-11-08 00:15:24
+LastEditors  : Hanqing Qi
+LastEditTime : 2023-11-10 17:40:48
+FilePath     : /GUI/SimpleGUI_V2/simpleGUIutils.py
+Description  : Some functions for the simpleGUI.py
+"""
+
+import matplotlib.widgets as widgets
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+from simpleGUIparams import *
+import numpy as np
+import math
+
+
+
